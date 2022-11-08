fc364433957004e0d6921bdd176c959934475002
katsuronim <rereandreas9@gmail.com>
2022-11-08 15:07:06 +0700

    Commit GD10_A_10610 API server

---
+++ app/Models/Modelmahasiswa.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Modelmahasiswa extends Model
+{
+    protected $table = 'mahasiswa';
+    protected $primaryKey = 'mhsnobp';
+    protected $allowedFields = [
+    'mhsnobp','mhsnama','mhsalamat','prodinama','mhstgllhr'
+    ];
+    
+    // protected $DBGroup          = 'default';
+    // protected $table            = 'mahasiswas';
+    // protected $primaryKey       = 'mhsnobp';
+    // protected $useAutoIncrement = true;
+    // protected $insertID         = 0;
+    // protected $returnType       = 'array';
+    // protected $useSoftDeletes   = false;
+    // protected $protectFields    = true;
+    // protected $allowedFields    = [];
+
+    // // Dates
+    // protected $useTimestamps = false;
+    // protected $dateFormat    = 'datetime';
+    // protected $createdField  = 'created_at';
+    // protected $updatedField  = 'updated_at';
+    // protected $deletedField  = 'deleted_at';
+
+    // // Validation
+    // protected $validationRules      = [];
+    // protected $validationMessages   = [];
+    // protected $skipValidation       = false;
+    // protected $cleanValidationRules = true;
+
+    // // Callbacks
+    // protected $allowCallbacks = true;
+    // protected $beforeInsert   = [];
+    // protected $afterInsert    = [];
+    // protected $beforeUpdate   = [];
+    // protected $afterUpdate    = [];
+    // protected $beforeFind     = [];
+    // protected $afterFind      = [];
+    // protected $beforeDelete   = [];
+    // protected $afterDelete    = [];
+}
